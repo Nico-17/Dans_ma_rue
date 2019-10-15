6a2198066c47a1ddd9d39fcd17e01955e1edd4d5
Nico-17 <couturiernicolas0@gmail.com>
2019-10-15 21:06:26 +0200

    Update delete.php

--- views/admin/defauts/delete.php
+++ views/admin/defauts/delete.php
@@ -1,11 +1,20 @@
 <?php
-//use App\Connection;
+use App\Connection;
 //use App\Table\DefautTable;
 
 //$pdo = Connection::getPDO();
 //$table = new DefautTable($pdo);
 //$table->delete($params['id']);
 //header('Location: ' . $router->url('admin_defauts') . '?delete=1');
+
+$pdo = Connection::getPDO;
+if(isset($_GET['id']) AND !empty($_GET['id'])) {
+   $suppr_id = htmlspecialchars($_GET['id']);
+   $suppr = $bdd->prepare('DELETE FROM defaut WHERE id = ?');
+   $suppr->execute(array($suppr_id));
+   header('Location: ' . $router->url('admin_defauts') . '?delete=1');
+}
+
 ?> 
 
  <h1>Suppression de <?= $params['id'] ?></h1>
